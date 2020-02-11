7ecad28536943e34d9898184663f6a8a7de2c7eb
Jason Smalridge <jasons@unity3d.com>
2020-02-11 12:28:04 -0500

    Working out landmark bounds

--- Assets/Scripts/Playhead.cs
+++ Assets/Scripts/Playhead.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Labs.MARS;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Playhead : MonoBehaviour
 {
     float speed = 5f;
     private Vector3 startPosition;
     private Vector3 endPosition;
+    public LandmarkController polygonLandmark;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +22,34 @@ public class Playhead : MonoBehaviour
         // Need to add a buffer value to append time to end position in be able to adjust loop
 
         // Make playhead invisible
-        GetComponent<Renderer>().enabled = false;
+        //GetComponent<Renderer>().enabled = false;
         startPosition = new Vector3(0,0,0);
         endPosition = new Vector3(10,0,0);
     }
-
+    
     void Update()
     {
-        UpdatePlayheadPosition(gameObject);
+        //UpdatePlayheadPosition(gameObject);
+        var edgeOutput = polygonLandmark.output as LandmarkOutputPolygon;
+        //var BoundingRect = edgeOutput.AvailableLandmarkDefinitions;
+        
+        if (edgeOutput == null)
+        {
+            // The assigned landmark is not set to type “edge”
+        }
+        else
+        {
+            // Do something with the data in edgeOutput, such
+            //Debug.Log(edgeOutput.pose + " -> " + edgeOutput.pose);
+            //Debug.Log(edgeOutput.localVertices.Count);
+        }
+    }
+
+    public void test(LandmarkController l)
+    {
+        var edgeOutput = l.output as LandmarkOutputPolygon;
+        Debug.Log(edgeOutput.pose + " -> " + edgeOutput.pose);
+        Debug.Log(edgeOutput.worldVertices.Count);
     }
 
     void UpdatePlayheadPosition(GameObject go)
@@ -35,11 +59,16 @@ public class Playhead : MonoBehaviour
 
         Debug.Log(Vector3.Distance(go.transform.position, endPosition).ToString());
 
-        if (Math.Abs(Vector3.Distance(go.transform.position, endPosition)) < 1)
+        if (Vector3.Distance(go.transform.position, endPosition) < 1)
         {
             go.transform.position = startPosition;
         }
 
+    }
+
+    public void GetParentSize()
+    {
+        // Can I get the size of the generated plane?
     }
 
 }
