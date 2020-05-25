f1a97b825e7ba02af09578115e0f02b03d451f25
Jason Smalridge <jasons@unity3d.com>
2020-05-25 16:46:13 -0400

    add image marker for sine.  Can rotate prefab to change tone

--- Assets/Scripts/PlayTone.cs
+++ Assets/Scripts/PlayTone.cs
@@ -15,6 +15,51 @@ public class PlayTone : MonoBehaviour
 
     public float gain = 0f;
 
+    private float _sensitivity;
+    private Vector3 _mouseReference;
+    private Vector3 _mouseOffset;
+    private Vector3 _rotation;
+    private bool _isRotating;
+     
+    void Start ()
+    {
+        _sensitivity = 0.4f;
+        _rotation = Vector3.zero;
+    }
+     
+    void Update()
+    {
+        if(_isRotating)
+        {
+            // offset
+            _mouseOffset = (Input.mousePosition - _mouseReference);
+             
+            // apply rotation
+            _rotation.y = -(_mouseOffset.x + _mouseOffset.y) * _sensitivity;
+             
+            // rotate
+            transform.Rotate(_rotation);
+             
+            // store mouse
+            _mouseReference = Input.mousePosition;
+        }
+    }
+     
+    void OnMouseDown()
+    {
+        // rotating flag
+        _isRotating = true;
+         
+        // store mouse
+        _mouseReference = Input.mousePosition;
+    }
+     
+    void OnMouseUp()
+    {
+        // rotating flag
+        _isRotating = false;
+    }
+
     private void OnAudioFilterRead(float[] data, int channels)
     {
         increment = frequency * 2.0 * Mathf.PI / sampling_frequency;
--- Assets/Scripts/Playhead.cs
+++ Assets/Scripts/Playhead.cs
@@ -44,6 +44,7 @@ public class Playhead : MonoBehaviour
             _init = true;
         }
         UpdatePlayHeadPosition();
+        
     }
 
     private void UpdatePlayHeadPosition()
